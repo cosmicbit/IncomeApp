d40505ca13ca57c42fcc9e3d82048b8467ef0c43
Philips Jose <philipsjose2003@gmail.com>
2025-10-17 12:33:49 +0530

    Initial Commit

---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Philips on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
